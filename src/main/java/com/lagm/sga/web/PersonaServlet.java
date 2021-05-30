d98fb2c99668e2cdeb31c11b21b5b6e448a56b96
Luis Guisado <lguisadom@gmail.com>
2021-05-30 01:14:52 -0500

    Se inyecta el EJB mediante @Inject del API CDI

--- src/main/java/com/lagm/sga/web/PersonaServlet.java
+++ src/main/java/com/lagm/sga/web/PersonaServlet.java
@@ -1,6 +1,10 @@
 package com.lagm.sga.web;
 
+import com.lagm.sga.domain.Persona;
+import com.lagm.sga.servicio.PersonaService;
 import java.io.IOException;
+import java.util.List;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/personas")
 public class PersonaServlet extends HttpServlet {
+    /* Notacion @Inject es parte del API CDI (Context Dependency Injection) permite inyectar componentes. En este caso 
+    para poder tener disponible una instancia del EJB Local por medio de la interfaz local PersonaService */
+    @Inject
+    PersonaService personaServiceEJB;
     
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
